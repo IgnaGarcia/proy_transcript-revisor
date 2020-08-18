1082db401e14d9cbf3c7cd70fee4d10a1b133282
Igna <gnachoxp@gmail.com>
2020-08-18 14:53:55 -0300

    Retoques para ser usado en cualquier lado

--- transcriptor.py
+++ transcriptor.py
@@ -4,9 +4,11 @@ from ibm_watson import SpeechToTextV1
 from ibm_watson.websocket import RecognizeCallback, AudioSource
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-_key = '' #Inserte la key aqui
+#Inserte la key aqui
+_key = ''
 
-_url = '' #Inserte la url de su clave aqui
+#Inserte la url de su clave aqui
+_url = '' 
 
 authenticator = IAMAuthenticator(_key)
 speech_to_text = SpeechToTextV1(
@@ -23,7 +25,9 @@ class MyRecognizeCallback(RecognizeCallback):
     def on_data(self, data):
         jsonDictonary = data #Guardar el resultado como Dictionary
         jsonText= json.dumps(jsonDictonary, indent=2)  #Convertir el resultado a un JSON Indentado para facil lectura
-
+        
+        print("\n\nGuardando resultado...")
+        
         f = open ("salida/data.txt", "w") #Archivo para todo el resultado (Timestamps, Alternatives, Keywords, ETC)
         f.write(jsonText) #Guardar el JSON Indentado
         f.close()
@@ -85,6 +89,7 @@ keywords= claves.split('-')
 with open('audio.'+tipo,'rb') as audio_file:
     audio_source = AudioSource(audio_file)
 
+    print("\n\nCargando datos...")
     speech_to_text.recognize_using_websocket(
         audio= audio_source,
         content_type= audioType,
