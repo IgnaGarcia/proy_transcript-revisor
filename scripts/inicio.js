46af3fcdcd246d989b383bc2cd3940ae75e9d53b
Igna Garcia <gnachoxp@gmail.com>
2021-04-11 00:48:56 -0300

    agrega creacion de revision e interfaz apra indexedDB

--- scripts/inicio.js
+++ scripts/inicio.js
@@ -1,4 +1,21 @@
-let crearRevision = () => {
+//Constructor de Revision
+let revisionFactory = (titulo, audio, texto) => {
+    //timestamp
+    let date = new Date(Date.now())
+    let strDate = date.getFullYear()+"/"+date.getMonth()+"/"+date.getUTCDate()+" "+date.getHours()+':'+date.getMinutes()
+    
+    let obj = {
+        titulo: titulo,
+        audio: audio,
+        texto: texto,
+        fecha: strDate
+    }
+
+    return obj
+}
+
+//Captar los datos ingresados, manipularlos y guardarlos
+let crearRevision = async () => {
     //Obtener los inputs
     let inputTitulo = document.getElementById("inputTitulo")
     let inputTexto = document.getElementById("inputTexto").files[0]
@@ -6,34 +23,66 @@ let crearRevision = () => {
 
     //Leer el JSON para guardarlo
     let json = 0
-    if (inputTexto) json = fileToJSON(inputTexto)
+    if (inputTexto) json = await fileToJSON(inputTexto)
 
     //Guardar el audio
     let audio = new Audio()
     audio.src = window.URL.createObjectURL(inputAudio)
+
+    //Crear Revision
+    let revision = revisionFactory(inputTitulo, audio, json)
 }
 
+//Promise Wraper para manager de la DB
+let promiseReq = req => {
+    return new Promise((resolve, reject) => {
+        req.onsuccess = () => resolve(req.result);
+        req.onerror = () => reject(req.error);
+    });
+}
+
+//Manager de la DB del browser
+let indexedDBManager = async () => {
+    // Abrimos la DB
+    let openRequest = indexedDB.open('testDB', 1);
+    openRequest.onupgradeneeded = (e) => {
+        let db = e.target.result;
+        db.createObjectStore("doc", { keyPath: "id", autoIncrement: true });
+        console.log("created indexed DB")
+    };
+
+    return promiseReq(openRequest).then(db => {
+        let obj = {db: db}
+        obj.transaction= obj.db.transaction("doc", "readwrite")
+        obj.store= obj.transaction.objectStore("doc")
+        obj.add= (item) => promiseReq(obj.store.add(item))
+        obj.get= (key) => promiseReq(obj.store.get(key))
+        obj.delete= (key) => promiseReq(obj.store.delete(key))
+        obj.put= (item) => promiseReq(obj.store.put(item))
+        obj.getAll= () => promiseReq(obj.store.getAll())
+        return obj
+    })
+}
+
+//Promesa de lectura completa del archivo
 let readFile = (file) => {
     return new Promise((resolve, reject) => {
-        const reader = new FileReader();
+        const reader = new FileReader()
 
         reader.onload = res => resolve(res.target.result)
         reader.onerror = err => reject(err)
 
-        reader.readAsText(file, "UTF-8");
+        reader.readAsText(file, "UTF-8")
     })
 }
 
+//Leer el archivo, convertirlo a JSON y pasarlo a limpio
 let fileToJSON = async (file) => {
-    let json = 0
-    readFile(file).
-        then(result => {
-            json = clearJSON(JSON.parse(result))
-        })
-
-    return json
+    let result = await readFile(file)
+    return await clearJSON(JSON.parse(result))
 }
 
+//Constructor de Palabras
 let wordFactory = (word, from, to, confidence) => {
     let obj = {
         word: word,
@@ -42,10 +91,10 @@ let wordFactory = (word, from, to, confidence) => {
         confidence: confidence,
         isKeyword: false
     }
-    
     return obj
 }
 
+//Constructor de Parrafos
 let paragraphFactory = (paragraph, from) => {
     let obj = {
         paragraph: paragraph,
@@ -54,10 +103,10 @@ let paragraphFactory = (paragraph, from) => {
         speaker: -1,
         words: []
     }
-
     return obj
 }
 
+//Limpieza de JSON de entrada
 let clearJSON = (json) => {
     let wordCount = 0
     let cleared = []
--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,46 @@
-window.addEventListener("load", () => {
-    let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, velit, quas quod, soluta quae impedit ut vel doloribus natus a dolorem repellendus laboriosam explicabo sint eligendi odit aperiam unde molestiae.";
+let promiseReq = req => {
+    return new Promise((resolve, reject) => {
+        req.onsuccess = () => resolve(req.result);
+        req.onerror = () => reject(req.error);
+    });
+}
+
+let indexedDBManager = async () => {
+    // Abrimos la DB
+    let openRequest = indexedDB.open('testDB', 1);
+    openRequest.onupgradeneeded = (e) => {
+        let db = e.target.result;
+        db.createObjectStore("doc", { keyPath: "id", autoIncrement: true });
+        console.log("created indexed DB")
+    };
+
+    return promiseReq(openRequest).then(db => {
+        let obj = {db: db}
+        obj.transaction= obj.db.transaction("doc", "readwrite")
+        obj.store= obj.transaction.objectStore("doc")
+        obj.add= (item) => promiseReq(obj.store.add(item))
+        obj.get= (key) => promiseReq(obj.store.get(key))
+        obj.delete= (key) => promiseReq(obj.store.delete(key))
+        obj.put= (item) => promiseReq(obj.store.put(item))
+        obj.getAll= () => promiseReq(obj.store.getAll())
+        return obj
+    })
+}
+
+window.addEventListener("load", async () => {
+    let managerIDB = await indexedDBManager()
+    console.log(managerIDB)
+    //managerIDB.add({"qcy": 20}).then((e) => console.log(e))
+    managerIDB.get(2).then(e => {
+        console.log(e)
+        e.qcy = "algo"
+        managerIDB.put(e).then(e => console.log(e))
+    })
+    managerIDB.getAll().then(e => console.log(e))
+    managerIDB.delete(3).then(e => console.log(e))
+
+
+    /*let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, velit, quas quod, soluta quae impedit ut vel doloribus natus a dolorem repellendus laboriosam explicabo sint eligendi odit aperiam unde molestiae.";
 
     let root = document.getElementById("root");
     root.setAttribute("contenteditable", "true")
@@ -39,5 +80,5 @@ window.addEventListener("load", () => {
     });
 
     // Save the PDF
-    doc.save('sample-document.pdf');
+    doc.save('sample-document.pdf');*/
 })
