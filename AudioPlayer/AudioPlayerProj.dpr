3ebd6f6dbe2adfe85b5711c6735a72656a08f283
Igna Garcia <gnachoxp@gmail.com>
2021-04-30 01:37:23 -0300

    pruebas locales delphi

---
+++ AudioPlayer/AudioPlayerProj.dpr
@@ -0,0 +1,14 @@
+program AudioPlayerProj;
+
+uses
+  Vcl.Forms,
+  AudioPlayer in 'AudioPlayer.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ TextEditor/TextEditorProj.dpr
@@ -0,0 +1,14 @@
+program TextEditorProj;
+
+uses
+  Vcl.Forms,
+  TextEditor in 'TextEditor.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
