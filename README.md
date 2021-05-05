a1c3f5a69cef6071fc5ea3309df0aee3ed237da2
Igna Garcia <gnachoxp@gmail.com>
2021-05-05 19:48:22 -0300

    pipenv y limpieza de json

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
 # SpeechToTextIBM
 Programa en Python que consuma API de IBM Watson para la transcripcion de archivos
 
-
-## 1- Instalar Python y marcar "Agregarl a PATH" 
+- **Instalar Python y marcar "Agregarl a PATH"**
 https://www.python.org/downloads/
 
+- **Instalar dependencias PIPENV y levantar entorno virtual, por cmd ejecutar**
+```
+pip install pipenv
 
-## 2-Instalar dependencias de IBM Watson, por cmd ejecutar	
-pip install --upgrade "ibm-watson>=4.5.0"
+pipenv install
+```
 
-
-## 3-Crearse una cuenta en 
+- **Crearse una cuenta en**
 https://cloud.ibm.com/
 
-y obtener una KEY y URL para el servicio
-https://cloud.ibm.com/catalog/services/speech-to-text
+    y obtener una KEY y URL para el servicio
+    https://cloud.ibm.com/catalog/services/speech-to-text
 
-
-## 4-Guardar el audio en la carpeta del repositorio
-IMPORTANTE: nombrar al archivo  "audio"
-
-
-## 5-Ejecutar
+- **Ejecutar**
+```
+pipenv run python transcriptor.py INPUT_DIR OUTPUT_DIR
+```
+---
+# Guia de uso para pipenv
+- **Agregar dependencias** 
+```
+pipenv install {PAQUETE}
+```
+- **Si agregas nuevas dependencias no te olvides de plasmarlas en el .lock**
+```
+pipenv lock 
+```
+- **Si queres borrar el entorno virtual**
+```
+pipenv --rm
+```
--- transcriptor.py
+++ transcriptor.py
@@ -1,101 +1,118 @@
 import json
+import time
+import sys
+import pathlib
+
 from os.path import join, dirname
 from ibm_watson import SpeechToTextV1
 from ibm_watson.websocket import RecognizeCallback, AudioSource
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import time
 
+def wordFactory(word, desde, to, confidence, speaker):
+    return {
+        'word': word,
+        'from': desde,
+        'to': to,
+        'confidence': confidence,
+        'speaker': speaker
+    }
+
+def paragraphFactory(desde):
+    return { 
+        'from': desde, 
+        'to': -1, 
+        'words': []
+    }
+
+def cleanJson(data):
+    wordCount = 0
+    cleared = []
+
+    for result in data['results']:
+        paragraph = paragraphFactory(result['alternatives'][0]['timestamps'][0][1])
+        i = 0
+
+        for word in result['alternatives'][0]['timestamps']:
+            if(len(result['alternatives'][0]['timestamps']) -1 == i):
+                word[0] = word[0]+"."
+            
+            nWord = wordFactory(word[0], word[1], word[2], result['alternatives'][0]['word_confidence'][i][1], data['speaker_labels'][wordCount]['speaker'])            
+            
+            paragraph['to'] = word[2]
+            paragraph['words'].append(nWord)
+            
+            wordCount += 1
+            i += 1  
+        cleared.append(paragraph)
+    return cleared
 
 class MyRecognizeCallback(RecognizeCallback):
     def __init__(self):
         RecognizeCallback.__init__(self)
 
     def on_data(self, data):
+        output_dir = sys.argv[2]
         jsonDictonary = data  # Guardar el resultado como Dictionary
         # Convertir el resultado a un JSON Indentado para facil lectura
         jsonText = json.dumps(jsonDictonary, indent=2)
 
-        print("\n\nGuardando resultado...")
+        print(f"\nGuardando resultado en: {output_dir}")
 
         # Archivo para todo el resultado (Timestamps, Keywords, ETC)
-        f = open("salida/data.json", "w")
-        f.write(jsonText)  # Guardar el JSON Indentado
+        f = open(f"{pathlib.Path(output_dir)}/data.json", "w")
+        f.write(jsonText) 
         f.close()
 
-        # Archivo solo para las posibles transcripciones
-        f = open("salida/transcripcion.txt", "w")
-        i = 0
-
-        for x in jsonDictonary['results']:
-            f.write(x['alternatives'][0]['transcript'])
+        f = open(f"{pathlib.Path(output_dir)}/clean.json", "w")
+        jsonText = json.dumps(cleanJson(jsonDictonary), indent=2)
+        f.write(jsonText) 
         f.close()
 
-        print("\n\nPeticion exitosa! revisar en la carpeta raiz del repositorio")
+        print(f"\nPeticion exitosa! revisar en {output_dir}")
 
     def on_error(self, error):
-        print('\n\nError : {}'.format(error))
+        print(f'\nError : {error}')
 
     def on_inactivity_timeout(self, error):
-        print('\n\nInactividad en el audio : {}'.format(error))
+        print(f'\nInactividad en el audio : {error}')
 
+def main():
+    # Inserte la key aqui
+    _key = 'IlPmRpIn1M-Twblnj0KF-J6N1e-nnhvxGhCEFGsebKDo'
+    # Inserte la url de su clave aqui
+    _url = 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/62725879-72ad-4e7b-a9bd-5597be5f13ec'
 
-tinicial = time.time()
+    input_dir = sys.argv[1]
 
-# Inserte la key aqui
-_key = 'IlPmRpIn1M-Twblnj0KF-J6N1e-nnhvxGhCEFGsebKDo'
+    authenticator = IAMAuthenticator(_key)
+    speech_to_text = SpeechToTextV1(authenticator=authenticator)
 
-# Inserte la url de su clave aqui
-_url = 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/62725879-72ad-4e7b-a9bd-5597be5f13ec'
+    speech_to_text.set_service_url(_url)
+    speech_to_text.set_disable_ssl_verification(True)
 
-authenticator = IAMAuthenticator(_key)
-speech_to_text = SpeechToTextV1(authenticator=authenticator)
+    myRecognizeCallback = MyRecognizeCallback()
 
-speech_to_text.set_service_url(_url)
-speech_to_text.set_disable_ssl_verification(True)
+    with open(pathlib.Path(sys.argv[1]), 'rb') as audio_file:
+        audio_source = AudioSource(audio_file)
 
-myRecognizeCallback = MyRecognizeCallback()
+        print(f"\nCargando datos\t\taudio path: {input_dir}")
+        speech_to_text.recognize_using_websocket(
+            audio = audio_source,
+            content_type = 'audio/' + pathlib.Path(sys.argv[1]).suffix[1:],
+            recognize_callback = myRecognizeCallback,
+            model = 'es-ES_BroadbandModel',
+            inactivity_timeout = 180,
+            smart_formatting = True,
+            speaker_labels = True,
+            timestamps = True,
+            word_confidence = True,
+            max_alternatives = 1)
 
-opcion = int(input(
-    "Seleccione el tipo de archivo: \n\t1-mp3 \n\t2-flac \n\t3-ogg \n\t4-wav \n: "))
-if opcion == 1:
-    audioType = 'audio/'+'mp3'
-    tipo = 'mp3'
-elif opcion == 2:
-    audioType = 'audio/'+'flac'
-    tipo = 'flac'
-elif opcion == 3:
-    audioType = 'audio/'+'ogg'
-    tipo = 'ogg'
-elif opcion == 4:
-    audioType = 'audio/'+'wav'
-    tipo = 'wav'
-else:
-    audioType = 'audio/'+'mp3'
-    tipo = 'mp3'
+if __name__ == "__main__":
+    tinicial = time.time()
+    main()    
+    tfinal = time.time()
+    difftime = tfinal - tinicial
 
-claves = input("Ingrese las palabras clave separadas por un - (guion)\n: ")
-keywords = claves.split('-')
-
-with open('audio.'+tipo, 'rb') as audio_file:
-    audio_source = AudioSource(audio_file)
-
-    print("\n\nCargando datos...")
-    speech_to_text.recognize_using_websocket(
-        audio=audio_source,
-        content_type=audioType,
-        recognize_callback=myRecognizeCallback,
-        model='es-ES_BroadbandModel',
-        keywords=keywords,
-        keywords_threshold=0.8,
-        inactivity_timeout=180,
-        smart_formatting=True,
-        speaker_labels= True,
-        timestamps=True,
-        word_confidence= True,
-        max_alternatives=1)
-
-tfinal = time.time()
-difftime = tfinal - tinicial
-print("La funcion tardo: ", difftime, "segs")
-
-input('\n\n>>>>Presione ENTER para salir')
+    print("\nLa funcion tardo: ", difftime, "segs")
+    input('\n>>>>Presione ENTER para salir')
