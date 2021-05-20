aba9a9bb3d17a94e442fd1dc627db42d1fcf81b4
Igna Garcia <gnachoxp@gmail.com>
2021-05-19 21:31:33 -0300

    vosk paragraph list

--- IBM-paragraphList/ibmlist.py
+++ IBM-paragraphList/ibmlist.py
@@ -1,6 +1,6 @@
 '''
 Usage: 
-    python plist.py -i <input_file> -o <output_file> [-s]
+    python ibmlist.py -i <input_file> -o <output_file> [-c] [-s]
 '''
 import json
 import sys, getopt
@@ -33,7 +33,22 @@ def speakersList(jsonDictonary, writer):
             writer.write(f"\nDiferencia con prox = {round(jsonDictonary[i+1]['from'] - p['to'], 2)}segs\n\n")        
 
         i += 1
-            
+
+def cleanList(jsonDictonary, writer):
+    i = True
+    for p in jsonDictonary:
+        if i:
+            parrafo = ""
+            i = False
+        else:
+            parrafo = "\n\n"
+
+        for w in p['words']:
+            word= w['word']
+            parrafo += f'{word} '
+
+        writer.write(parrafo)       
+         
 
 def simpleList(jsonDictonary, writer):
     i=0
@@ -55,14 +70,16 @@ def simpleList(jsonDictonary, writer):
 
         i += 1
 
-def main(ifile, ofile, s):
+def main(ifile, ofile, s, c):
 
     reader = open(ifile, 'r')
     jsonDictonary = json.loads(reader.read())
     reader.close()
 
     writer = open(ofile, 'w')
-    if s:
+    if c:
+        cleanList(jsonDictonary, writer)
+    elif s:
         speakersList(jsonDictonary, writer)
     else:
         simpleList(jsonDictonary, writer)
@@ -72,17 +89,18 @@ def main(ifile, ofile, s):
 if __name__ == "__main__":
     argv = sys.argv[1:]
 
-    short_options = "i:o:s"
-    long_options = ["input_file=", "output_file=", "speaker"]
+    short_options = "i:o:sc"
+    long_options = ["input_file=", "output_file=", "speaker", "clean"]
 
     inputfile = ''
     outputfile = ''
     speaker = False
+    clean = False
 
     try:
         opts, args = getopt.getopt(argv, short_options, long_options)
     except getopt.GetoptError:
-        print('\nERROR- usar plist.py -i <input_file> -o <output_file> [-s]')
+        print('\nERROR- usar python ibmlist.py -i <input_file> -o <output_file> [-c] [-s]')
         sys.exit(2)
 
     for opt, arg in opts:
@@ -92,6 +110,8 @@ if __name__ == "__main__":
             outputfile = arg
         elif opt in ("-s", "--speaker"):
             speaker = True
+        elif opt in ("-c", "--clean"):
+            clean = True
     
     if inputfile == '':
         print('\nERROR-La direccion del input es requerida.\nDeclarala en el llamado con -i o --input_file')
@@ -100,4 +120,4 @@ if __name__ == "__main__":
         print('\nINFO-No se especifica direccion del output, se guardara junto al ejetubale\n')
         outputfile = "./plist.txt"
 
-    main(inputfile, outputfile, speaker)
+    main(inputfile, outputfile, speaker, clean)
